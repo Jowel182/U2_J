194ce5eb0f026df9b6976b26e9284b1d004fa0de
Joel Pimentel <j.pimentel@a1qa.com>
2024-02-23 17:59:39 -0600

    modified FileDownload test

--- src/test/java/selenium/FileDownloadTest.java
+++ src/test/java/selenium/FileDownloadTest.java
@@ -2,13 +2,26 @@ package selenium;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.*;
+import static org.awaitility.Duration.*;
+import static java.util.concurrent.TimeUnit.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 
 public class FileDownloadTest extends BaseTest {
     private final By FILE_DOWNLOAD = By.xpath(String.format(PRECISE_TEXT_XPATH, "File Download"));
-    private final String FILE_NAME = "Test.txt";
+    private final String FILE_NAME = "testfile.txt";
     private final By FILE_NAME_XPATH = By.xpath(String.format(PRECISE_TEXT_XPATH, FILE_NAME));
+    private final String FILE_PATH = RELATIVE_RESOURCE_PATH + FILE_NAME;
+    private final File downloadedFile = new File(FILE_PATH);
+
 
 
     @Test
@@ -16,8 +29,31 @@ public class FileDownloadTest extends BaseTest {
         driver.findElement(FILE_DOWNLOAD).click();
         Assert.assertTrue(driver.findElement(FILE_NAME_XPATH).isDisplayed(), "File is not displayed");
         driver.findElement(FILE_NAME_XPATH).click();
+        Assert.assertTrue(isFileExists(downloadedFile), "File is not downloaded");
         // todo: assert file is downloaded
     }
 
+    private boolean isFileExists(File file){
+
+        try{
+
+            Awaitility.await().atMost(MAX_WAIT, TimeUnit.SECONDS).unitl(file::exists);
+
+        }
+        catch (ConditionTimeoutException exception){
+            return false;
+
+        }
+        return true;
+
+    }
     // todo: delete file
+
+    public void deleteFile(){
+
+        if (downloadedFile.exists()) {
+            downloadedFile.delete();
+        }
+
+    }
 }
