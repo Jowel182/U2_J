15a5baa7c45936f892cd17fb3ded57a7f0838bef
Joel Pimentel <j.pimentel@a1qa.com>
2024-02-26 22:43:04 -0600

    Modified Frames Test

--- src/test/java/selenium/FramesTest.java
+++ src/test/java/selenium/FramesTest.java
@@ -14,6 +14,8 @@ public class FramesTest extends BaseTest {
     private final By EDIT = By.xpath(String.format(PRECISE_TEXT_XPATH, "Edit"));
     private final By UNDO = By.xpath(String.format(PRECISE_TEXT_XPATH, "Undo"));
     private final String TEXT_IS_NOT_DISPLAYED_MSG = "Text is not displayed";
+    private final String IFRAME_ID = "mce_0_ifr";
+    private final By TEXT_FIELD = By.id("tinymce");
 
 
     @Test
@@ -21,10 +23,19 @@ public class FramesTest extends BaseTest {
         driver.findElement(FRAMES).click();
         driver.findElement(IFRAME).click();
         //todo: input text to the text field
+
+        driver.switchTo().frame(IFRAME_ID);
+        driver.findElement(TEXT_FIELD).sendKeys(RANDOM);
         Assert.assertTrue(driver.findElement(By.xpath(String.format(PRECISE_TEXT_XPATH, INIT_TEXT + RANDOM))).isDisplayed(),
                 TEXT_IS_NOT_DISPLAYED_MSG);
+
+        driver.switchTo().defaultContent();
         driver.findElement(EDIT).click();
         driver.findElement(UNDO).click();
         // todo: assert text is not displayed
+
+        driver.switchTo().frame(IFRAME_ID);
+        Assert.assertTrue(driver.findElement(By.xpath(String.format(PRECISE_TEXT_XPATH, INIT_TEXT))).isDisplayed(),TEXT_IS_NOT_DISPLAYED_MSG);
+
     }
 }
